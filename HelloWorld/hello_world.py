22bd4bf69081cbd62b389d230eebe80f1054292b
A1dy <aldien@gmail.com>
2020-04-14 21:19:13 +0700

    Begin

---
+++ HelloWorld/hello_world.py
@@ -0,0 +1,17 @@
+car_model = "Truck"
+car_color = "black"
+car_year = 1987
+car_number_of_doors = 2
+
+print ("Car Info:\nModel: " + car_model + "\nColor: " + car_color+ "\nYear: " + str(car_year) + "\nNumber of Doors: " + str(car_number_of_doors))
+
+car_color = "white"
+print ("Car Info:\nModel: " + car_model + "\nColor: " + car_color+ "\nYear: " + str(car_year) + "\nNumber of Doors: " + str(car_number_of_doors))
+
+age_human_1 = 1
+age_human_2 = 12
+age_human_3 = 36
+age_human_4 = 59
+age_human_5 = 82
+count_of_people = 5
+print ((age_human_1 + age_human_2 + age_human_3 + age_human_4 + age_human_5) / count_of_people)
